cd68a96ad6b4a783962c8ba8b083aa92d5cc36f0
KMaina <maina.ken0@gmail.com>
2018-11-14 14:02:26 +0300

    [Chore #161950928] Fix linting issues in base.js

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,17 +1,17 @@
-function errorNotification(res){
-    return `<p>${res.body.error}</p>`;
+function errorNotification(res) {
+  return `<p>${res.body.error}</p>`;
 }
 
-function successNotification(res){
-    return `<p>${res.body.success}</p>`;
+function successNotification(res) {
+  return `<p>${res.body.success}</p>`;
 }
 
-function setToken(res){
-    localStorage.setItem("token", res.body.access_token)
+function setToken(res) {
+  localStorage.setItem('token', res.body.access_token);
 }
 
-function getToken(){
-    return localStorage.getItem('token')
+function getToken() {
+  return localStorage.getItem('token');
 }
 
-let url = 'https://ken-online-store.herokuapp.com/api/v2/'
+const url = 'https://ken-online-store.herokuapp.com/api/v2/';
