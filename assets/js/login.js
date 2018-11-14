2fd37bcf4f6a6ef369153503453cbf1f9998d448
KMaina <maina.ken0@gmail.com>
2018-11-14 15:37:24 +0300

    [Chore #161950928] Remove import in the login.js file

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,7 +1,3 @@
-import {
-  errorNotification, setToken, url,
-} from './base';
-
 const userError = document.getElementById('user-error');
 let error = '';
 const loginUser = document.getElementById('LoginUser');
@@ -12,18 +8,17 @@ function LoginUser(e) {
   const userName = document.getElementById('name').value;
   const userPassword = document.getElementById('password').value;
 
-  fetch(`${url} + auth/login`, {
+  fetch(`${url}auth/login`, {
     method: 'POST',
     headers: {
       Accept: 'application/json',
-      'Content-type': 'application/json',
+      'Content-type': 'application/json'
     },
     body: JSON.stringify({ name: userName, password: userPassword }),
   })
     .then(res => res.json().then(data => ({ status: res.status, body: data })))
     .then((result) => {
       error = errorNotification(result);
-
       if (result.status === 200) {
         setToken(result);
         window.location.href = 'pages/profile.html';
