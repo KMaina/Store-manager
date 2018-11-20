f6076c1af4705d46061a88450c2b46a677bede45
KMaina <maina.ken0@gmail.com>
2018-11-20 12:24:07 +0300

    [Chore #161950928] Fix linting issues in get_product.js

--- assets/js/get_product.js
+++ assets/js/get_product.js
@@ -1,19 +1,19 @@
 window.onload = function getProducts(e) {
-    e.preventDefault();
-    fetch('https://ken-online-store.herokuapp.com/api/v2/products',{
-        method:'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-type':'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Request-Method': 'GET'
-        }
-    })
-    .then((res) => res.json())
+  e.preventDefault();
+  fetch('https://ken-online-store.herokuapp.com/api/v2/products', {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Request-Method': 'GET'
+    }
+  })
+    .then(res => res.json())
     .then((data) => {
-        let products = data.products;
-        let products_table = document.getElementById('showProducts');
-        th = `
+      let products = data.products;
+      let products_table = document.getElementById('showProducts');
+      th = `
         <tr>
         <th>Product ID</th>
         <th>Name</th>
@@ -22,18 +22,18 @@ window.onload = function getProducts(e) {
         <th>Amount</th>
         <th>Add</th>
         </tr>
-        `
-        products_table.innerHTML = th
-        products.forEach(product => { 
-            products_table.innerHTML += '<tr>'+
-                '<td>'+product.product_id+'</td>'+
-                '<td>'+product.product_name+'</td>'+
-                '<td>'+product.product_cost+'</td>'+
-                '<td>'+product.quantity+'</td>'+
-                '<td><input type="text" class="form-text" placeholder="Amount" id="sale"></td>'+
-                '<td><input type="submit" class="submit" value="Add"></td>'+
-                '</tr>';
-        })
+        `;
+      products_table.innerHTML = th
+      products.forEach(product => {
+        products_table.innerHTML += '<tr>'+
+        '<td>'+product.product_id+'</td>'+
+        '<td>'+product.product_name+'</td>'+
+        '<td>'+product.product_cost+'</td>'+
+        '<td>'+product.quantity+'</td>'+
+        '<td><input type="text" class="form-text" placeholder="Amount" id="sale"></td>'+
+        '<td><input type="submit" class="submit" value="Add"></td>'+
+        '</tr>';
+    })
     })
     .catch((err) => console.log(err)) // display in notification
-}
+};
