98b3c482af6eed9f89a231d31fc8d758a629875c
KMaina <maina.ken0@gmail.com>
2018-11-20 12:05:56 +0300

    [Chore #161950928] Fix linting issues in register.js

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,38 +1,48 @@
 document.getElementById('registerUser').addEventListener('submit', registerUser);
 
-function registerUser(e){
-    e.preventDefault();
-    let user_name = document.getElementById('name').value;
-    let user_password = document.getElementById('password').value;
-    let user_confirm = document.getElementById('confirm').value;
+const userError = document.getElementById('user-error');
+const userSuccess = document.getElementById('user-success');
+let error = '';
+let success = '';
+const accessToken = `Bearer ${getToken()}`;
 
-    fetch(url + 'auth/signup', {
-        method:'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-type':'application/json',
-            'Authorization': 'Bearer ' + getToken()
-        },
-        body:JSON.stringify({name:user_name, password:user_password, confirm:user_confirm})
+function registerUser(e) {
+  e.preventDefault();
+  const userName = document.getElementById('name').value;
+  const userPassword = document.getElementById('password').value;
+  const userConfirm = document.getElementById('confirm').value;
+  // Add a place to add a user as an admin
+
+  fetch(`${url}auth/signup`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json',
+      Authorization: accessToken
+    },
+    body: JSON.stringify({
+      name: userName,
+      password: userPassword,
+      confirm: userConfirm
     })
-    .then(res =>  res.json().then(data => ({status: res.status, body: data})))
+  })
+    .then(res => res.json().then(data => ({ status: res.status, body: data })))
     .then((result) => {
-        let error = errorNotification(result);
-        let success = successNotification(result);
-        if(result.status == 201){
-            document.getElementById('user-success').innerHTML = success;
-            setTimeout(() => {
-                let success = "";
-                document.getElementById('user-error').innerHTML = success;
-                window.location.href = 'profile.html'
-            }, 5000)
-        }
-        else{
-            document.getElementById('user-error').innerHTML = error;
-            setTimeout(() => {
-                let error = "";
-                document.getElementById('user-error').innerHTML = error;
-            }, 5000)
-        }
-    })
+      error = errorNotification(result);
+      success = successNotification(result);
+      if (result.status === 201) {
+        userSuccess.innerHTML = success;
+        setTimeout(() => {
+          success = '';
+          userSuccess.innerHTML = success;
+          window.location.href = 'profile.html';
+        }, 5000);
+      } else {
+        userError.innerHTML = error;
+        setTimeout(() => {
+          error = '';
+          userError.innerHTML = error;
+        }, 5000);
+      }
+    });
 }
